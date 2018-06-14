af58f573159ab2f7058880200d2301c5dd57b984
Mike <mayres@bhtp.com>
2018-06-14 10:43:22 -0500

    merge in typings from the old typescript < 2.0 implementation to support 2.0 directly in lib. from https://github.com/typed-contrib/node-amqp10

---
+++ typings/amqp10/policies/qpid_java_policy.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for node-amqp10 v3
+// Project: https://github.com/noodlefrenzy/node-amqp10
+// Definitions by: Maxime LUCE <https://github.com/SomaticIT/>
+// Definitions: https://github.com/typed-contrib/node-amqp10
+
+import Policy = require("./policy");
+
+declare const policy: Policy;
+export = policy;
